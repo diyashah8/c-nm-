b20c387defae9d90168e5f794d451464559617a8
Diya Shah <cclloouudd19@gmail.com>
2025-09-07 22:16:23 +0545

    finding the root of f(x) = e^(-x) - 3x

---
+++ secant method3.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x) {
+    return exp(-x) - 3*x;
+}
+
+float root(float x1, float x2) {
+    float x3;
+    x3 = x2 - ((f(x2) * (x2 - x1)) / (f(x2) - f(x1)));
+    return x3;
+}
+
+int main() {
+    float x1, x2, E, Ear, x3;
+    int i = 1;
+
+    printf("Enter the value of x1 and x2: ");
+    scanf("%f %f", &x1, &x2);
+
+    printf("Enter the value of the error: ");
+    scanf("%f", &E);
+
+    printf("\nIter\tx1\t\tx2\t\tf(x1)\t\tf(x2)\t\tx3\t\tEar\n");
+    printf("----------------------------------------------------------------\n");
+
+    do {
+        x3 = root(x1, x2);
+        printf("%d\t%f\t%f\t%f\t%f\t%f\t%f\n", i, x1, x2, f(x1), f(x2), x3, Ear);
+
+        x1 = x2;
+        x2 = x3;
+
+        if (x2 != 0)
+            Ear = fabs((x2 - x1) / x2);
+        else
+            Ear = fabs(x2 - x1);
+
+        i++;
+    } while (Ear > E);
+
+    printf("\nThe root is %f\n", x3);
+    return 0;
+}
+
